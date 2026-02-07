75ebd373b797f2dd25c3ca81d9992032f7a039cd
justnorriel <fabrianorriel@gmail.com>
2026-02-07 17:51:06 +0800

    chore: setup environment variables for supabase and fix user view import

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://kjljqvhvvlqgdhflbycr.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqbGpxdmh2dmxxZ2RoZmxieWNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0MTA4MzUsImV4cCI6MjA4NTk4NjgzNX0.iRcCNldpQQRXdaxaWHcUreGls67LsEAZsAv2G6mdRFY'
+// This looks for variables in your .env file locally OR Vercel Settings online
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
